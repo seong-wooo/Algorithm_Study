ac5f40ca16ad29c38f41c16544d8c3afa8c712a0
seongwoo <seongwoo0513@uos.ac.kr>
2021-10-10 18:40:16 +0900

    DP_1로만들기

---
+++ 이코테2021/다이나믹프로그래밍/DP_1로만들기.py
@@ -0,0 +1,16 @@
+# 1로 만들기
+
+x = int(input())
+dp = [0,0] + (x-1)*[0]
+
+for i in range(2, x+1):
+  minimum = dp[i-1] + 1
+  if i % 5 == 0:
+    minimum = min(minimum, dp[i//5]+1)
+  if i % 3 == 0:
+    minimum = min(minimum, dp[i//3]+1)
+  if i % 2 == 0:
+    minimum = min(minimum, dp[i//2]+1)
+  dp[i] = minimum
+
+print(dp[x])
