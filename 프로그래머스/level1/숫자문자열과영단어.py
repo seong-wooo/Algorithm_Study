57f540ccf8809f9c0fca0e10be59aa1a016a9f47
seongwoo <seongwoo0513@uos.ac.kr>
2021-11-05 02:30:19 +0900

    Solve 숫자문자열과영단어

---
+++ 프로그래머스/level1/숫자문자열과영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+    for i in range(10):
+        if num[i] in s:
+            s = s.replace(num[i], str(i))
+    return int(s)
