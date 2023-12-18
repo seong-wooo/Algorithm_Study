6d52be8ef2fb2852015e971dc32711ac05ebe69e
박성우 <seongwoo0513@uos.ac.kr>
2023-12-18 10:09:14 +0900

    [level 2] Title: 귤 고르기, Time: 17.64 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,32 +1,12 @@
 from collections import Counter
 
 def solution(k, tangerine):
-    tangerine = [0] + list(sorted(Counter(tangerine).values()))
+    answer = 0
+    for t in sorted(Counter(tangerine).values(), reverse = True):
+        k -= t
+        answer += 1
+        if k <= 0:
+            return answer
+    return -1
 
-    for i in range(1, len(tangerine)):
-        tangerine[i] += tangerine[i - 1]
-        
-
-    minimum = 100000
     
-    left = 0
-    right = 1
-
-
-    while left <= right and right < len(tangerine):
-        total = tangerine[right] - tangerine[left] 
-        
-        if total == k:
-            minimum = min(right - left, minimum)
-            left += 1
-            right += 1
-            
-        elif total > k:
-            minimum = min(right - left, minimum)
-            left += 1
-        
-        else:
-            right += 1
-    
-    
-    return minimum
