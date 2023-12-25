84ee7bf89383463c6df5913b10f844d97d9d610d
박성우 <seongwoo0513@uos.ac.kr>
2023-12-26 02:20:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,4 +1,4 @@
-from collections import deque, defaultdict
+from collections import deque
 def solution(s):
     change = {
         "(": "0",
@@ -17,26 +17,17 @@ def solution(s):
 
 
 def do(s):
-    d = defaultdict(deque)
+    q = deque()
     
     for i in range(len(s)):
         c = s[i]
         
         if c in "135":
             target = str(int(c) - 1)
-            if not d[target]:
+            if not q or q[-1] != target:
                 return 0
-            
-            for o in "024":
-                if target != o and d[o] and d[o][-1] > d[target][-1]:
-                    return 0
-                
-            d[target].pop()
+            q.pop()
         else:
-            d[c].append(i)
-
-    for q in d.values():
-        if q:
-            return 0
+            q.append(c)
     
-    return 1
+    return 1 if not q else 0
