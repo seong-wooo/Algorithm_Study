c935fa678d35c6bc142a20ec8e0b510bcfacbbcf
박성우 <seongwoo0513@uos.ac.kr>
2023-12-27 03:13:44 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 63.54 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,37 +1,28 @@
 def solution(arr):
 
-    result = press(arr)
-    return [result.count(0), result.count(1)]
-
+    answer = [0, 0]
     
-def press(arr):
-    if is_all_same(arr):
-        return [arr[0][0]]
+    def is_all_same(arr, y, x, l):
+        target = arr[y][x]    
+        for j in range(y, y + l):
+            for i in range(x, x + l):
+                if arr[j][i] != target:
+                    return False
+        return True
     
-    l = len(arr)    
-    arrs = [[] for _ in range(4)]
-    for top in arr[:l//2]:
-        arrs[0].append(top[:l//2])
-        arrs[1].append(top[l//2:])
+    
+    def press(arr, y, x, l):
+        if is_all_same(arr, y, x, l):
+            answer[arr[y][x]] += 1
+            return
         
-    for bottom in arr[l//2:]:
-        arrs[2].append(bottom[:l//2])
-        arrs[3].append(bottom[l//2:])
-    
-    result = []
-    
-    for a in arrs:
-        result += press(a)
-    
-    return result
-    
-    
-
-def is_all_same(arr):
-    target = arr[0][0]    
-    for j in range(len(arr)):
-        for i in range(len(arr[0])):
-            if arr[j][i] != target:
-                return False
-    return True
         
+        press(arr, y, x, l // 2)
+        press(arr, y + l // 2, x, l // 2)
+        press(arr, y, x + l // 2, l // 2)
+        press(arr, y + l // 2, x + l // 2, l // 2)
+        
+    
+    press(arr, 0, 0, len(arr))
+    
+    return answer
