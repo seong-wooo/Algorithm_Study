2d5eadb9984f0a010dbac4608de76ced594813a5
박성우 <seongwoo0513@uos.ac.kr>
2023-12-26 22:55:35 +0900

    [level 2] Title: 방문 길이, Time: 0.36 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -14,8 +14,8 @@ def solution(dirs):
         ny, nx = y + dy, x + dx
         
         if 0 <= ny < 11 and 0 <= nx < 11:
-            if (y, x, ny, nx) not in visited and (ny, nx, y, x) not in visited:
-                visited.add((y, x, ny, nx))
+            visited.add((y, x, ny, nx))
+            visited.add((ny, nx, y, x))
             y, x = ny, nx
     
-    return len(visited)
+    return len(visited) // 2
