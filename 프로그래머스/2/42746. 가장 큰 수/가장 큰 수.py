a2c403deb3cd8a196ee8fe59855bf440da3ed4e6
박성우 <seongwoo0513@uos.ac.kr>
2024-01-04 12:08:26 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    answer = "".join(sorted(map(str, numbers), key = lambda x : (x * 4)[:4] , reverse = True))
+    if answer[0] == "0":
+        return "0"
+    return answer
