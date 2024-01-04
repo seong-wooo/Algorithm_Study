efc10971066095e15c7bfd16f9ead6d27c600722
박성우 <seongwoo0513@uos.ac.kr>
2024-01-04 13:16:58 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,6 +1,6 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 13:6:29
+2024년 1월 4일 13:16:55
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<int[]> q = new LinkedList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+            q.add(new int[]{priorities[i], i});
+        }
+        
+        int answer = 1;
+        System.out.println(q.peek()[0] == -pq.peek());
+        // while(!q.isEmpty()) {
+        //     if (q.peek()[0] == -pq.peek()) {
+        //         int[] t = q.poll();
+        //         if (t[1] == location) {
+        //             return answer;
+        //         }
+        //         pq.poll();
+        //         answer++;
+        //     } 
+        //     else {
+        //        q.add(q.poll()); 
+        //     }
+        // }
+        
+        return answer;
+    }
+}
