6f20c42b71a3a130dc7eb48d08ca6491f9d48de0
박성우 <seongwoo0513@uos.ac.kr>
2024-01-06 02:27:41 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.38 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -1,15 +1,23 @@
 class Solution {
     public int solution(int n) {
+        int d = 1_000_000_007;
+        n /= 2;
         int[] dp = new int[n + 1];
-        dp[2] = 3;
-        dp[4] = 11;
+        dp[1] = 3;
+        dp[2] = 11;
         
-        for (int i = 6; i <= n; i += 2) {
-            dp[i] = (2 + dp[i - 2] * 3) % 1000_000_007;
-            for (int j = 4; j < i; j += 2) {
-                dp[i] += (dp[i - j] * 2) % 1000_000_007;
-                dp[i] %= 1000_000_007;
-            }
+        int sum = dp[1];
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] += 2 + dp[i-1] * 2;
+            dp[i] %= d;
+            dp[i] += dp[i-1];
+            dp[i] %= d;
+            dp[i] += sum;
+            dp[i] %= d;
+            dp[i] += sum;
+            dp[i] %= d;
+            sum += dp[i - 1];
+            sum %= d;
         }
         
         return dp[n];
