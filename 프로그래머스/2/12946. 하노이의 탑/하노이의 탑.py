0cd0ba24ef08b05dd3098d9e3b1267f2d2f67a4d
박성우 <seongwoo0513@uos.ac.kr>
2024-01-05 20:43:55 +0900

    [level 2] Title: 하노이의 탑, Time: 15.42 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,17 +1,11 @@
 def solution(n):
-    answer = []
-    hanoi(1, 3, n, answer)
+    return hanoi(1, 3, n)
     
-    return answer
-    
-def hanoi(a, b, n, answer):
+def hanoi(a, b, n):
     if n == 1:
-        answer.append([a, b])
-        return
+        return [[a, b]]
     
-    hanoi(a, 6-a-b, n-1, answer)
-    hanoi(a, b, 1, answer)
-    hanoi(6-a-b, b, n-1, answer)
+    return hanoi(a, 6-a-b, n-1) + hanoi(a, b, 1) + hanoi(6-a-b, b, n-1)
     
 
     
