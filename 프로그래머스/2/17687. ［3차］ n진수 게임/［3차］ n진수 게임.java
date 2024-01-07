d3c219966f3e6f035e9373afa41766c9c63236bf
박성우 <seongwoo0513@uos.ac.kr>
2024-01-08 00:57:33 +0900

    [level 2] Title: [3차] n진수 게임, Time: 19.90 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,41 +1,17 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        String[] arr = new String[16];
-        for (int i = 0; i < 10; i++) {
-            arr[i] = String.valueOf(i);
-        }
-        
-        arr[10] = "A";
-        arr[11] = "B";
-        arr[12] = "C";
-        arr[13] = "D";
-        arr[14] = "E";
-        arr[15] = "F";
         int maxLength = m * (t - 1) + p;
         
         String base = "";
         int num = 0;
         while (base.length() < maxLength) {
-            base += toN(num, n, arr);
-            num++;
+            base += Integer.toString(num++, n);
         }
         
         StringBuilder sb = new StringBuilder();
         for (int i = p - 1; i < maxLength; i += m) {
             sb.append(base.charAt(i));
         }
-        return sb.toString();
+        return sb.toString().toUpperCase();
     }
-    
-    public String toN(int num, int n, String[] arr) {    
-        StringBuilder sb = new StringBuilder();
-        while (num >= n) {
-            sb.append(String.valueOf(arr[num % n]));
-            num /= n;
-        }
-        sb.append(String.valueOf(arr[num]));
-        
-        return sb.reverse().toString();
-    }
-    
 }
