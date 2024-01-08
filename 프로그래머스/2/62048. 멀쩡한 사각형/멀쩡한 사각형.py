15ec0ecc66a1ef9b8d0020af133eb5de8f5a4f30
박성우 <seongwoo0513@uos.ac.kr>
2024-01-08 09:05:52 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,8 +1,3 @@
+import math
 def solution(w,h):
-    return w * h - (w + h - gcd(w, h)) 
-
-
-def gcd(a, b):
-    if b == 0:
-        return a
-    return gcd(b, a % b)
+    return w * h - (w + h - math.gcd(w, h)) 
