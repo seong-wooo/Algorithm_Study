31ca51e3cc4ae07db0cf92492622c928d16daa5f
박성우 <seongwoo0513@uos.ac.kr>
2024-01-08 12:08:36 +0900

    [level 2] Title: 문자열 압축, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -5,16 +5,16 @@ class Solution {
         for (int length = 1; length < s.length() / 2 + 1; length++) {
             int i = 0;
             int result = s.length();
-            
-            while (i <= s.length() - length * 2) {
+            while (i <= s.length() - length * 2) {                
                 int press = 0;
-                while (i <= s.length() - length * 2 && s.substring(i, i+length).equals(s.substring(i+length, i+2*length))) {
+                String target = s.substring(i, i+length);
+                while (i <= s.length() - length * 2 && target.equals(s.substring(i+length, i+2*length))) {
                     i += length;
                     press++;
                 }
                 
                 if (press > 0) {
-                    result -= press * length - String.valueOf(press+1).length();
+                    result -= press * length - Integer.toString(press+1).length();
                 } else {
                     i += length;
                 }
