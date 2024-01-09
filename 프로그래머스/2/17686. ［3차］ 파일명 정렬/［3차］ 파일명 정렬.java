df9e0aae1a2cd18647f3a23c5cd35fe601db977f
박성우 <seongwoo0513@uos.ac.kr>
2024-01-10 03:51:54 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 14.63 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -14,10 +14,8 @@ class Solution {
             if (!ma.group(1).equals(mb.group(1)) ) {
                 return ma.group(1).compareTo(mb.group(1));
             }
-            if (!ma.group(2).equals(mb.group(2))) {
-                return Integer.parseInt(ma.group(2)) - Integer.parseInt(mb.group(2));
-            }
-            return 0;
+
+            return Integer.parseInt(ma.group(2)) - Integer.parseInt(mb.group(2));
         });
         
         return files;
