af551259ac35483dc14e8dae953326d451aa2836
박성우 <seongwoo0513@uos.ac.kr>
2024-01-10 03:02:43 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.32 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,10 +1,6 @@
 import re
 
 def solution(files):
-    files = [re.split(r"(\d+)",file) for file in files]
-    
-    files.sort(key = lambda x : (x[0].lower(), int(x[1])))
-    
-    return [file[0] + str(file[1]) + "".join(file[2:]) for file in files]
+    return [file[0] + str(file[1]) + "".join(file[2:]) for file in sorted([re.split(r"(\d+)",file) for file in files], key = lambda x : (x[0].lower(), int(x[1])))]
         
         
