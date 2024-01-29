16c28a402a96058305aa7940fa036c8d884e8d61
박성우 <seongwoo0513@uos.ac.kr>
2024-01-29 13:55:11 +0900

    [Bronze II] Title: 농구 경기, Time: 64 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.py
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -2,12 +2,6 @@ import sys
 from collections import Counter
 
 input = sys.stdin.readline
-
 names = Counter([input()[0] for _ in range(int(input()))])
-
-answer = []
-for name in names:
-    if names[name] >= 5:
-        answer.append(name)
-
+answer = [name for name in names if names[name] >= 5]
 print("".join(sorted(answer)) if answer else "PREDAJA")
