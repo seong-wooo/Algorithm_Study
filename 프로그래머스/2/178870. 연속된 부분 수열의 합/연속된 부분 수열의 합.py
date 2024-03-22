e01bdbb1edc4051d3ec9c768d63c12f6124a643f
박성우 <tjddn0513@gmail.com>
2024-03-22 21:43:20 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,35 +1,31 @@
-# 투 포인터 방식으로 푼다.
-
-from collections import defaultdict
-
-def solution(sequence, k):    
-    d = defaultdict(list)
+def solution(sequence, k):
+    answer = []
+    length = len(sequence)
     
     left = 0
     right = 0
-    current = sequence[left]
+    current = sequence[0]
     
-    while left <= right and right < len(sequence):
-        
+    while right < len(sequence):
         if current == k:
-            if not d[right - left]:
-                d[right - left] = [left, right]
-            
+            if length > right - left:
+                answer = [left, right]
+                length = right - left
             current -= sequence[left]
             left += 1
             right += 1
             if right < len(sequence):
                 current += sequence[right]
-        
+            
         elif current > k:
             current -= sequence[left]
             left += 1
+            
         
         else:
             right += 1
             if right < len(sequence):
                 current += sequence[right]
-                
-
-    return d[min(d.keys())]
+    
+    return answer
         
