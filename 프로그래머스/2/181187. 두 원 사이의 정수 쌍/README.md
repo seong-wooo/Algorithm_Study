7a3cf88bd0914e733dbd864ae6dcbe07bec7e36f
박성우 <tjddn0513@gmail.com>
2024-03-22 21:26:20 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 434.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 원 사이의 정수 쌍 - 181187 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187?language=python3) 
 
 ### 성능 요약
 
-메모리: 81.1 MB, 시간: 8.61 ms
+메모리: 10.1 MB, 시간: 434.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 12:0:19
+2024년 03월 22일 21:26:17
 
 ### 문제 설명
 
--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,31 +1,18 @@
-# 축을 제외한 1사분면에서의 점 개수를 구한다.
-# 구한 값 * 4 + 축의 점 * 4
-
-# x = 1 ~ r1 - 1 일 때 .. 개수
-# ---
-# x = r1 ~ r2 - 1 일 때 개수
-
-# y = sqrt(r^2 - x^2)
-
 import math
 
 def solution(r1, r2):
-    
     answer = 0
-    
+    r12 = r1 ** 2
+    r22 = r2 ** 2
+
     for x in range(1, r1):
-        r1_max = math.sqrt(r1**2 - x**2)
+        x2 = x ** 2
+        r1_max = math.sqrt(r12 - x2)
         r1_max = r1_max - 1 if r1_max % 1 == 0 else int(r1_max)
-            
-        r2_max = int(math.sqrt(r2**2 - x**2))
-        
-        answer += r2_max - r1_max
+        answer += int(math.sqrt(r22 - x2)) - r1_max
     
     for x in range(r1, r2):
-        answer += int(math.sqrt(r2**2 - x**2))
-        
+        answer += int(math.sqrt(r22 - x ** 2))
+    
     answer *= 4
-    
-    answer += (r2 - r1 + 1) * 4
-    
-    return answer
+    return answer + (r2 - r1 + 1) * 4
