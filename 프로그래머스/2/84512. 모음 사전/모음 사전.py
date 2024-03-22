949358fb7a542d0a33871e57118d5f607c41ab25
박성우 <tjddn0513@gmail.com>
2024-03-22 21:55:16 +0900

    [level 2] Title: 모음 사전, Time: 0.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,11 @@
-from itertools import product as pd
+from itertools import product
 
 def solution(word):
     result = []
     for i in range(1, 6):
-        for p in pd("AEIOU", repeat = i):
+        for p in product("AEIOU", repeat = i):
             result.append("".join(p))
     
     result.sort()
     
     return result.index(word) + 1
-        
-        
-    
