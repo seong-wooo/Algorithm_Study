952501ba14b08af9042c3d15a5584c6727cd0c21
박성우 <tjddn0513@gmail.com>
2024-09-21 12:13:05 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.60 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -1,23 +1,28 @@
 class Solution {
     public int solution(int n) {
-        int d = 1_000_000_007;
         n /= 2;
+        int d = 1_000_000_007;
+        
         int[] dp = new int[n + 1];
         dp[1] = 3;
-        dp[2] = 11;
+        dp[2] = dp[1]*3 + 2;
+        int s = dp[1];
         
-        int sum = dp[1];
-        for (int i = 3; i < n + 1; i++) {
-            dp[i] += 2 + dp[i-1] * 2;
+        for (int i=3; i<n+1; i++) {
+            dp[i] += 2;
             dp[i] %= d;
             dp[i] += dp[i-1];
             dp[i] %= d;
-            dp[i] += sum;
+            dp[i] += dp[i-1];
             dp[i] %= d;
-            dp[i] += sum;
+            dp[i] += dp[i-1];
             dp[i] %= d;
-            sum += dp[i - 1];
-            sum %= d;
+            dp[i] += s;
+            dp[i] %= d;
+            dp[i] += s;
+            dp[i] %= d;
+            s += dp[i-1];
+            s %= d;
         }
         
         return dp[n];
--- 프로그래머스/2/12902. 3 x n 타일링/README.md
+++ 프로그래머스/2/12902. 3 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 3 x n 타일링 - 12902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12902?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.43 ms
+메모리: 52 MB, 시간: 0.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 12:05:16
+2024년 09월 21일 12:13:03
 
 ### 문제 설명
 
