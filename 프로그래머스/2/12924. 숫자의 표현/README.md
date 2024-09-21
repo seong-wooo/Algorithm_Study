5292846906c5c2cdfa8ba6a1d8eddfbab6d48bfb
박성우 <tjddn0513@gmail.com>
2024-09-21 13:54:55 +0900

    [level 2] Title: 숫자의 표현, Time: 0.46 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 0.95 ms
+메모리: 10.1 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 7:27:34
+2024년 09월 21일 13:54:53
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,15 +1,2 @@
 def solution(n):
-    nums = [ i * (i + 1) // 2 for i in range(n//2 + 2)]
-
-    answer = 0
-    for right in range(1, n//2 + 2):
-        left = right - 1
-        while left > 0 and nums[right] - nums[left] < n:
-            left -= 1
-            
-        if nums[right] - nums[left] == n:
-            answer += 1
-
-    
-    return answer + 1 if n > 2 else answer
-            
+    return len([i for i in range(1, n+1, 2) if n % i == 0])
