80a48451676e96c24e1450f370be221c038b2b0e
박성우 <tjddn0513@gmail.com>
2024-09-21 13:26:53 +0900

    [level 2] Title: 땅따먹기, Time: 207.36 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.6 MB, 시간: 15.45 ms
+메모리: 32.3 MB, 시간: 207.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 10:19:8
+2024년 09월 21일 13:26:51
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,10 +1,9 @@
 def solution(land):
-    for j in range(len(land) - 2, -1 , -1):
-        for i in range(len(land[j])):
-            land[j][i] += max(land[j+1][:i] + land[j+1][i+1:])
     
-    
+    for j in range(len(land) - 2, -1, -1):
+        for i in range(len(land[0])):
+            land[j][i] += max(land[j + 1][:i] + land[j+1][i+1:])
+
     return max(land[0])
-            
-            
-            
+
+
