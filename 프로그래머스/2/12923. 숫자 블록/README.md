62fc8dca8afd46ea589e4a22c15fcba3e2f20331
박성우 <tjddn0513@gmail.com>
2024-09-22 00:54:30 +0900

    [level 2] Title: 숫자 블록, Time: 1875.43 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/README.md
+++ 프로그래머스/2/12923. 숫자 블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 블록 - 12923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923) 
 
 ### 성능 요약
 
-메모리: 52.6 MB, 시간: 850.97 ms
+메모리: 10.6 MB, 시간: 1875.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 8:37:37
+2024년 09월 22일 00:54:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
@@ -1,10 +1,23 @@
+# 약수를 구한다
+# 약수 중 자기 제외 가장 큰 수를 넣는다
+
 def solution(begin, end):
-    answer = [0] + [1]* (end - begin) if begin == 1 else [1] * (end - begin + 1)
+    answer = [0] * (end - begin + 1)
     
     
-    for i in range(begin, end + 1):
-        for n in range(2, min(10000000, int(i ** 0.5) + 1)):
-            if i % n == 0:
-                answer[i - begin] = max(answer[i-begin], i // n) if i // n <= 10000000 else max(answer[i-begin], n)
+    for n in range(begin, end + 1):
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                r = n // i
+                if r <= 10_000_000:
+                    answer[n-begin] = r
+                    break
+                else:
+                    answer[n-begin] = i
+                
+                
+        if answer[n-begin] == 0 and n > 1:
+            answer[n-begin] = 1
     
     return answer
+    
