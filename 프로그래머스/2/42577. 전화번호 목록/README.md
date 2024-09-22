9be3d52194ee16fbc68e704f06d1b319473eff29
박성우 <tjddn0513@gmail.com>
2024-09-22 23:59:29 +0900

    [level 2] Title: 전화번호 목록, Time: 459.39 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 28.8 MB, 시간: 209.33 ms
+메모리: 42.8 MB, 시간: 459.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 17일 19:55:53
+2024년 09월 22일 23:59:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,9 @@
 def solution(phone_book):
-    s = set()
+    phone = set()
     
-    for p in sorted(phone_book, key=lambda x : len(x)):
-        for i in range(1, len(p) + 1):
-            if p[:i] in s:
+    for p in sorted(phone_book, key = lambda x : len(x)):
+        for i in range(1, len(p)):
+            if p[:i] in phone:
                 return False
-        s.add(p)
+        phone.add(p)
     return True
