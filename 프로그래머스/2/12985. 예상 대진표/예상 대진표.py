30c006028300f253c085028c7f25ca98ce301b3d
박성우 <tjddn0513@gmail.com>
2024-09-22 14:44:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,11 +1,10 @@
 def solution(n,a,b):
     answer = 1
-    a -= 1
-    b -= 1
+    a, b = min(a, b), max(a, b)
     
-    while a // 2 != b // 2:
-        a //= 2
-        b //= 2
+    while a % 2 != 1 or b % 2 != 0 or a + 1 != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
         answer += 1
         
     return answer
