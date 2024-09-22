10591711eff4fefbd6ee1788418b836e6ffb73b2
박성우 <tjddn0513@gmail.com>
2024-09-22 14:34:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,10 +1,10 @@
 def solution(n, words):
-    s = set()
-    s.add(words[0])
+    w = set()
+    w.add(words[0])
+    
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0] or words[i] in s:
+        if words[i] in w or words[i-1][-1] != words[i][0]:
             return [i % n + 1, i // n + 1]
-        s.add(words[i])
+        w.add(words[i])
         
     return [0, 0]
-    
