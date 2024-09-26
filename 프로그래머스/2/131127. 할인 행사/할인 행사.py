1b28cae31ba446bbf4b93838a2b0c13f557e395b
박성우 <tjddn0513@gmail.com>
2024-09-27 02:40:32 +0900

    [level 2] Title: 할인 행사, Time: 43.69 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,29 +1,24 @@
 from collections import defaultdict
-
 def solution(want, number, discount):
     w = defaultdict(int)
-    for i in range(len(want)):
-        w[want[i]] += number[i]
     
+    for i in range(len(want)):
+        w[want[i]] = number[i]
     
     d = defaultdict(int)
-    for i in range(10):
-        d[discount[i]] += 1
+    for j in range(10):
+        d[discount[j]] += 1
     
-    result = contains(w, d)
-    for i in range(1, len(discount) - 9):
-
-        d[discount[i - 1]] -= 1
-        d[discount[i + 9]] += 1
-        if d[discount[i - 1]] == 0:
-            d.pop(discount[i -1])
-        result += contains(w, d)
-        
-    return result
-        
+    answer = 0 if w != d else 1
     
-def contains(w, d):
-    for v in w:
-        if not d[v] or w[v] > d[v]:
-            return 0
-    return 1
+    for k in range(1, len(discount) - 9):
+        d[discount[k-1]] -= 1
+        if d[discount[k-1]] == 0:
+            del d[discount[k-1]]
+        d[discount[k+9]] += 1
+       
+        if w == d:
+            answer += 1
+        
+        
+    return answer
