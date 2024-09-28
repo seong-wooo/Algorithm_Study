23999059f1b76766aafb29f6952b0ab8f67f2d17
박성우 <tjddn0513@gmail.com>
2024-09-28 20:52:13 +0900

    [level 2] Title: 미로 탈출, Time: 16.44 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,72 +1,60 @@
-# 출발 -> 레버 -> 문
-# 두 번의 bfs를 돌리자
-# 1. 출발 to 레버
-# 2. 레버 to 문 
+# S -> L -> E
 from collections import deque
-
 def solution(maps):
-    start = ()
-    lebber = ()
-    exit = ()
     
+    y_l, x_l, count = find_l(maps)
+    
+    if y_l == -1:
+        return -1
+    
+    
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    q = deque()
+    
+    dy = [0,0,1,-1]
+    dx = [1,-1,0,0]
+    q.append((y_l, x_l, count))
+    visited[y_l][x_l] = True
+    while q:
+        y, x, count = q.popleft()
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+
+            if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and not visited[ny][nx] and maps[ny][nx] != "X":
+                if maps[ny][nx] == "E":
+                    return count + 1
+                visited[ny][nx] = True
+                q.append((ny, nx, count + 1))
+
+    return -1
+    
+    
+def find_l(maps):
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    q = deque()
+    
+    dy = [0,0,1,-1]
+    dx = [1,-1,0,0]
     
     for j in range(len(maps)):
         for i in range(len(maps[0])):
             if maps[j][i] == "S":
-                start = (j, i)
-            elif maps[j][i] == "L":
-                lebber = (j, i)
-            elif maps[j][i] == "E":
-                exit = (j, i)
-    
-    first = bfs(start, lebber, maps)
-    if first == -1:
-        return -1
-    
-    second = bfs(lebber, exit, maps)
-    
-    if second == -1:
-        return -1
-    
-    return first + second
-    
-
-def bfs(start, end, maps):
-    s_y, s_x = start
-    e_y, e_x = end
-    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
-    
-    q = deque()
-    q.append((s_y, s_x, 0))
-    visited[s_y][s_x] = True
-    dy = [0,0,1,-1]
-    dx = [1,-1,0,0]
-    
-    while q:
-        y, x, count = q.popleft()
+                visited[j][i] = True
+                q.append((j, i, 0))
         
-        count += 1
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            
-            if (ny, nx) == (e_y, e_x):
-                return count
-            
-            if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and not visited[ny][nx] and maps[ny][nx] != "X":
-                visited[ny][nx] = True
-                q.append((ny, nx, count))
-    
-    return -1
-    
+                while q:
+                    y, x, count = q.popleft()
+                    for k in range(4):
+                        ny = y + dy[k]
+                        nx = x + dx[k]
+                        
+                        if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and not visited[ny][nx] and maps[ny][nx] != "X":
+                            if maps[ny][nx] == "L":
+                                return (ny, nx, count + 1)
+                            visited[ny][nx] = True
+                            q.append((ny, nx, count + 1))
+    return (-1,-1,-1)
+                    
+                    
                 
-                
-                
-            
-        
-        
-    
-    
-    
-    
-    
