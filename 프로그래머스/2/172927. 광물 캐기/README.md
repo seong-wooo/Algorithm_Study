92e7011d22041fb2ea69cb9753d3310db64aaf68
박성우 <tjddn0513@gmail.com>
2024-09-29 02:17:38 +0900

    [level 2] Title: 광물 캐기, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 5.90 ms
+메모리: 10.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 3:3:43
+2024년 09월 29일 02:17:35
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,27 +1,21 @@
-import math
+from math import ceil
 
 def solution(picks, minerals):
-    total = sum(picks) * 5
-    minerals = minerals[:total if len(minerals) > total else len(minerals)]
+    d = {"diamond": 25, "iron": 5, "stone": 1}
+    minerals = list(map(lambda x: d[x], minerals))
+    score_minerals = sorted(list(enumerate([sum(minerals[i:i+5]) for i in range(0, len(minerals), 5)]))[:sum(picks)], reverse =True, key=lambda x: x[1])
+    
+    answer = 0
+    m = 0 
+    for i in range(len(picks)):
+        for j in range(picks[i]):
+            index = score_minerals[m][0] * 5
 
-    m = {k: v for k,v in zip(["diamond", "iron", "stone"], [25, 5, 1])}
-    minerals_value = [[0] * 5 for _ in range(math.ceil(len(minerals) / 5))]
-    
-    for i in range(len(minerals)):
-        c, l = i // 5, i % 5
-        minerals_value[c][l] = m[minerals[i]]
-    
-    minerals_value.sort(key=lambda x: sum(x), reverse = True)
-    
-    result = 0
-    damage = [25, 5, 1]
-    for values in minerals_value:
-        for j in range(3):
-            if picks[j]:
-                result += sum(map(lambda v: math.ceil(v / damage[j]), values))
-                picks[j] -= 1
-                break
-            
+            for k in range(index, min(index+5, len(minerals))):
+                answer += ceil(minerals[k] / (5**(2-i)))
                 
-        
-    return result
+            m += 1
+            
+            if m == len(score_minerals):
+                return answer
+    return answer
