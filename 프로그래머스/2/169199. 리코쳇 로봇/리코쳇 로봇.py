720e581f7d1da5e4e8fa06f2c9540c80d8882306
박성우 <tjddn0513@gmail.com>
2024-09-29 01:31:15 +0900

    [level 2] Title: 리코쳇 로봇, Time: 10.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,48 +1,33 @@
 from collections import deque
 
 def solution(board):
-    r_y = 0
-    r_x = 0
-    g_y = 0 
-    g_x = 0
-    for j in range(len(board)):
-        for i in range(len(board[0])):
-            if board[j][i] == "R":
-                r_y = j
-                r_x = i
-            
-            if board[j][i] == "G":
-                g_y = j
-                g_x = i
-                
-    q = deque()
-    q.append((r_y, r_x, 0))
+    visited = [[False] * len(board[0]) for _ in range(len(board))]
     
     dy = [0,0,1,-1]
     dx = [1,-1,0,0]
     
-    visited = [[False] * len(board[0]) for _ in range(len(board))]
-    visited[r_y][r_x] = True
+    q = deque()
     
-    while q:
-        r_y, r_x, count = q.popleft()
-        count += 1
-        
-        for i in range(4):
-            n_y, n_x = r_y, r_x
-            while 0 <= n_y + dy[i] < len(board) and 0 <= n_x + dx[i] < len(board[0]) and board[n_y + dy[i]][n_x + dx[i]] != "D":
-                n_y += dy[i]
-                n_x += dx[i]
-            
-            if (n_y, n_x) == (g_y, g_x):
-                return count
-            
-            if not visited[n_y][n_x]:
-                visited[n_y][n_x] = True
-                q.append((n_y, n_x, count))
-    return -1                
-            
-            
-            
-            
-        
+    for j in range(len(board)):
+        for i in range(len(board[0])):
+            if board[j][i] == "R":
+                visited[j][i] = True
+                q.append((j, i, 0))
+                
+                while q:
+                    y, x, count = q.popleft()
+                    
+                    for k in range(4):
+                        ny = y
+                        nx = x
+                        
+                        while 0 <= ny + dy[k] < len(board) and 0 <= nx + dx[k] < len(board[0]) and board[ny+dy[k]][nx+dx[k]] != "D":
+                            ny += dy[k]
+                            nx += dx[k]
+                        
+                        if not visited[ny][nx]:
+                            if board[ny][nx] == "G":
+                                return count + 1
+                            visited[ny][nx] = True
+                            q.append((ny, nx, count+1))
+    return -1
