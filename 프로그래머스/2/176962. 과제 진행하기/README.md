e5d55267c78176ed297eb253d48e831ea20a2daa
박성우 <tjddn0513@gmail.com>
2024-09-29 02:57:57 +0900

    [level 2] Title: 과제 진행하기, Time: 1.82 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/README.md
+++ 프로그래머스/2/176962. 과제 진행하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 과제 진행하기 - 176962 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962?language=python3) 
 
 ### 성능 요약
 
-메모리: 82.2 MB, 시간: 12.76 ms
+메모리: 10.7 MB, 시간: 1.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 13:51:50
+2024년 09월 29일 02:57:55
 
 ### 문제 설명
 
--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,68 +1,34 @@
 from collections import deque
 
 def solution(plans):
-    plans = deque(sorted(map(change_time, plans), key = lambda x : (x[1], x[2])))
-    rest = deque()
-    result = []
+    plans = list(sorted(map(change_time, plans), key=lambda x:x[1]))
+    plans += [['', plans[-1][1] + plans[-1][2], 0]]
     
-    current = [plans[0][1], plans[0][2]]
-    
-
-    while plans:
-        plan = plans.popleft()
-        # 현재 시각과의 차이를 구해서
-        d = difference(current[0], current[1], plan[1], plan[2])
+    answer = []
+    stack = deque()
+    for i in range(len(plans)-1):
+        time = min(plans[i+1][1] - plans[i][1], plans[i][2])
+        plans[i][2] -= time
         
-        # 현재, 시작 시각이 안되었다면
-        if d > 0:
-            # rest 에서 시간을 보낸다
-            while d > 0 and rest:
-                if rest[0][3] > d:
-                    rest[0][3] -= d
-                    break
-                else:
-                    d -= rest[0][3]
-                    result.append(rest.popleft()[0])
-            
-
-        # 시작 시각이 되었다면
-        current = [plan[1], plan[2]]
-        # 다음 시작 시각과 비교하여
-        if plans:
-            next_plan = plans[0]
-            d = difference(plan[1], plan[2], next_plan[1], next_plan[2])
-
-            # 끝낼 수 있으면 끝내고 시각을 옮긴다.
-            
-            if d >= plan[3]:
-                current = add_time(current[0], current[1], plan[3])
-                result.append(plan[0])
-
-            # 못 끝낸다면 rest 에 넣는다.
-            else:
-                current = add_time(current[0], current[1], d)
-                rest.appendleft([plan[0], plan[1], plan[2], plan[3] - d])
-
-
-        # 다음 시작이 없으면 그냥 결과에 넣는다.
+        if plans[i][2] > 0:
+            stack.append([plans[i][0], plans[i][2]])
         else:
-            result.append(plan[0])
-    
-    for r in rest:
-        result.append(r[0])
-    
-    return result
+            answer.append(plans[i][0])            
+            rest = plans[i+1][1] - plans[i][1] - time
             
+            while rest > 0 and stack:
+                if stack[-1][1] > rest:
+                    stack[-1][1] -= rest
+                    break
+                name, t = stack.pop()
+                rest -= t
+                answer.append(name)
+    
+    return answer + list(map(lambda x: x[0], stack))[::-1]
             
+
 def change_time(plan):
     h, m = map(int, plan[1].split(":"))
-    return [plan[0], h, m, int(plan[2])]
-
-
-def difference(h1, m1, h2, m2):
-    return (h2 - h1) * 60 + m2 - m1
-
-def add_time(h1, m1, minute):
-    new_m = m1 + minute
     
-    return [h1 + new_m // 60, new_m % 60]
+    return [plan[0], h*60+m, int(plan[2])]
+    
