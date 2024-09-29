c474ad87daa2e010cbf9104366df16e109b3b5b1
박성우 <tjddn0513@gmail.com>
2024-09-29 12:32:54 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.92 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,28 +1,24 @@
-from collections import defaultdict
+# 교집합크기 / 합집합크기
+# 둘 다 공집합일 때 = 1
 
+from collections import Counter
 
+import re
 def solution(str1, str2):
-    d1 = split(str1)    
-    d2 = split(str2)
+    c1 = Counter()
+    c2 = Counter()
     
-    if not d1 and not d2:
-        return 65536
+    for i in range(len(str1) - 1):
+        if str1[i:i+2].isalpha():
+            c1[str1[i:i+2].upper()] += 1
         
-    k1 = d1.keys()
-    k2 = d2.keys()
+    for j in range(len(str2) - 1):
+        if str2[j:j+2].isalpha():
+            c2[str2[j:j+2].upper()] += 1
     
-    inters, union = k1&k2, k1|k2
+    if not c1 and not c2:
+        return 65536
     
-    return (65536 * sum([min(d1[k],d2[k]) for k in inters])) // sum(max(d1[k], d2[k]) for k in union)
+    return int(sum((c1&c2).values()) / sum((c1|c2).values()) * 65536)
+        
     
-    
-    
-def split(str):
-    d = defaultdict(int)
-    
-    for i in range(len(str) - 1):
-        target = str[i:i+2]
-        if target.isalpha():
-            d[target.upper()] += 1
-    
-    return d
