a5fd47930838d21c4ff77a755d85dc254177ed2a
박성우 <tjddn0513@gmail.com>
2024-10-01 10:34:45 +0900

    [level 2] Title: 타겟 넘버, Time: 588.75 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.43 ms
+메모리: 99.4 MB, 시간: 588.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 3:42:19
+2024년 10월 01일 10:34:44
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,26 @@
-import sys
+from collections import deque
 
 def solution(numbers, target):
-    sys.setrecursionlimit(10**6)
+    q = deque()
+    q.append((-1, 0))
+    answer =0
+    while q:
+        index, result = q.popleft()
+        
+        if index == len(numbers) - 1:
+            if result == target:
+                answer += 1
+            continue
+            
+        next_index = index + 1
+        next_num = numbers[next_index]
+        q.append((next_index, result+next_num))
+        q.append((next_index, result-next_num))
+    return answer       
+            
+                
+        
     
-    return answer(numbers, target, 0, 0)
-
-
-def answer(numbers, target, index, current):
-    if index == len(numbers):
-        return 1 if target == current else 0
     
-    return answer(numbers, target, index + 1, current + numbers[index]) + answer(numbers, target, index + 1, current - numbers[index])
+    
+    
