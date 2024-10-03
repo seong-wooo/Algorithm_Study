086f90a4de1917c8f7db0951eeee8d43d315f8d6
박성우 <tjddn0513@gmail.com>
2024-10-03 15:47:36 +0900

    [level 2] Title: 삼각 달팽이, Time: 138.37 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=python3) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 17.99 ms
+메모리: 43.2 MB, 시간: 138.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 1:46:2
+2024년 10월 03일 15:47:33
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,24 +1,20 @@
 def solution(n):
-    answer = [[0] * i for i in range(1, n + 1)]
+    result = [[0] * i for i in range(1, n+1)]
+    direction = [(1, 0), (0, 1), (-1,-1)]
+    d = 0
+    count = n
+    i = 1
+    y, x = -1, 0
     
-    dy = [1, 0, -1]
-    dx = [0, 1, -1]
-    
-    node = 1
-    y, x = 0, 0
-    count = 0
-    
-    while count < n:
-        for i in range(n - count):
-            answer[y][x] = node
-            node += 1
-            if i != n - count - 1:
-                y += dy[count % 3]
-                x += dx[count % 3]
-        count += 1
-        y += dy[count % 3]
-        x += dx[count % 3]
-
-    
-    return sum(answer, [])
-    
+    while count > 0:
+        for round_count in range(count):
+            y,x = y + direction[d][0], x + direction[d][1]
+            result[y][x] = i
+            i += 1
+        count -= 1
+        d = (d+1)%3    
+        
+    answer =[]
+    for r in result:
+        answer += r
+    return answer
