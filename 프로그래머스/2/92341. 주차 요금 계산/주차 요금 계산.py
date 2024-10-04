d121cdaef1f64785f3fcd5d37d84a14a71f6f247
박성우 <tjddn0513@gmail.com>
2024-10-04 22:05:12 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,31 +1,36 @@
 from collections import defaultdict
 import math
-
 def solution(fees, records):
-    times = defaultdict(int)
-    park = dict()
+    r = defaultdict(list)
     
     for record in records:
-        time, num, command = record.split()
-        minute = change_time(time)    
-        
-        if command == "IN":
-            park[num] = minute
-        
-        else:
-            in_time = park[num]
-            times[num] += minute - in_time
-            del park[num]
-        
-    max_time = change_time("23:59")
-    for car in park:
-        times[car] += max_time - park[car]
+        re = record.split(" ")
+        h, m = re[0].split(":")
+        time = int(h) * 60 + int(m)
+        r[int(re[1])].append(time)
     
-    return [fees[1] + math.ceil(max(0, (times[car] - fees[0])) / fees[2]) * fees[3]  for car in sorted(times.keys())]
+    answer = []
+    for key in sorted(r.keys()):
+        times = r[key]
+        if len(times) % 2 == 1:
+            times += [23 * 60 + 59]
+        
+        time = 0
+        for i in range(0, len(times), 2):
+            time += times[i+1] - times[i]
+            
+        time -= fees[0]
+        result = fees[1]
+        
+        if time > 0:
+            result += math.ceil(time/fees[2]) * fees[3]
+        
+        answer.append(result)
+    return answer
         
         
         
-
-def change_time(time):
-    h, m = map(int, time.split(":"))
-    return 60 * h + m
+        
+        
+        
+        
