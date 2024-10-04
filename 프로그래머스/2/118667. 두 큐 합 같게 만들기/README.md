66406fcb7bcd35e8f074baf41140cf7ac6b7c815
박성우 <tjddn0513@gmail.com>
2024-10-04 22:48:47 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 71.01 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=python3) 
 
 ### 성능 요약
 
-메모리: 87.1 MB, 시간: 15.90 ms
+메모리: 37.6 MB, 시간: 71.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 23:19:55
+2024년 10월 04일 22:48:44
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,28 +1,28 @@
-from collections import deque
-
 def solution(queue1, queue2):
-    t_q1 = sum(queue1)
-    t_q2 = sum(queue2)
+    q1 = sum(queue1)
+    q2 = sum(queue2)
     
-    if (t_q1 + t_q2) % 2 != 0:
+    total = q1 + q2
+    if total % 2 != 0:
         return -1
     
-    q1 = deque(queue1)
-    q2 = deque(queue2)
+    target = total // 2
     
-    result = 0
-    while q1 and q2 and t_q1 != t_q2 and result < len(queue1) * 4:
-        if t_q1 > t_q2:
-            p = q1.popleft()
-            t_q1 -= p
-            t_q2 += p
-            q2.append(p)
+    q = queue1 + queue2
+    start = 0
+    end = len(queue1) - 1
+    answer = 0
+    
+    while q1 != target and start <= end:
+        if q1 < target:
+            end += 1
+            if end == len(q)-1:
+                return -1
+            q1 += q[end]
         else:
-            p = q2.popleft()
-            t_q1 += p
-            t_q2 -= p
-            q1.append(p)
+            q1 -= q[start]
+            start += 1
+        answer += 1
+    return answer 
         
-        result += 1
-        
-    return result if t_q1 == t_q2 else -1
+            
