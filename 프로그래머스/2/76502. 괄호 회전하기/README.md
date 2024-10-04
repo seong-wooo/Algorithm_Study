cbf6a22038de37431a2ae9678912b9214a03ab0a
박성우 <tjddn0513@gmail.com>
2024-10-04 10:58:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 146.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 8.95 ms
+메모리: 10.4 MB, 시간: 146.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 2:36:55
+2024년 10월 04일 10:58:49
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,33 +1,59 @@
+# 회전을 통해 s를 올바른 문자열로 만든다
+# s안에 올바른 문자열이 몇 개 있는지 확인한다
+
 from collections import deque
 def solution(s):
-    change = {
-        "(": "0",
-        ")": "1",
-        "{": "2",
-        "}": "3",
-        "[": "4",
-        "]": "5"
-    }
-    s = "".join(map(lambda x: change[x], s))
+    
+    for _ in range(len(s)):
+        if not is_right(s):
+            s = s[1:] + s[0]
+
+    if not is_right(s):
+        return 0
+    
+    print(s)
+    left = 0
+    right = 1
     answer = 0
-    for i in range(len(s)):
-        s = s[1:] + s[0]
-        answer += do(s)
+    
+    while right < len(s):
+        if is_right(s[left:right+1]):
+            answer += 1
+            left = right + 1
+            right = left + 1
+            continue
+        
+        right += 1
+    
     return answer
+    
+            
+        
+        
 
-
-def do(s):
+def is_right(s):
     q = deque()
     
-    for i in range(len(s)):
-        c = s[i]
+    for c in s:
+        if not q:
+            if c == ")" or c == "}" or c == "]":
+                return False
+            q.append(c)
+            continue
         
-        if c in "135":
-            target = str(int(c) - 1)
-            if not q or q[-1] != target:
-                return 0
+        if c == ")" and q[-1] == "(":
+            q.pop()
+        elif c == "}" and q[-1] == "{":
+            q.pop()
+        elif c == "]" and q[-1] == "[":
             q.pop()
         else:
             q.append(c)
     
-    return 1 if not q else 0
+    return not q
+        
+        
+        
+        
+            
+        
