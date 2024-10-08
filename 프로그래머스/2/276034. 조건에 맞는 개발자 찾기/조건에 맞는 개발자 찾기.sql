1259b0fc0bf1df2db3db4e35887956a66d595e5f
박성우 <tjddn0513@gmail.com>
2024-10-08 12:30:45 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+
+
+select id, email, first_name, last_name
+from developers
+where skill_code&(select code 
+    from skillcodes
+    where name = 'Python') > 0 or skill_code&(select code 
+    from skillcodes
+    where name = 'C#') > 0
+order by 1
