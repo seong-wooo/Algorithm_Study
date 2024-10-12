87de413d465fa4960929c74ed639e0aa6800d24e
박성우 <tjddn0513@gmail.com>
2024-10-12 22:18:46 +0900

    [level 2] Title: 더 맵게, Time: 1531.60 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 1650.35 ms
+메모리: 123 MB, 시간: 1531.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 00:24:11
+2024년 10월 12일 22:18:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,25 +1,23 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        Queue<Integer> pq = new PriorityQueue<Integer>();
-        int answer = 0;
-        for (int s : scoville) {
-            pq.add(s);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int sc : scoville) {
+            pq.add(sc);
         }
+        
+        int answer = 0;
         
         while (pq.peek() < K) {
-            int s1 = pq.poll();
+            int first = pq.poll();
             if (pq.isEmpty()) {
                 return -1;
-            }
-            int s2 = pq.poll();
-            
-            pq.add(s1 + s2*2);
+            }            
+            pq.add(first + pq.poll()*2);
             answer++;
         }
-        
-        return answer;
+        return answer;   
     }
 }
