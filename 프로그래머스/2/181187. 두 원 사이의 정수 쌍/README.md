6bd81878f02eca66cbcae162398ae75187c9edb7
박성우 <tjddn0513@gmail.com>
2024-10-12 11:18:59 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 23.00 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 원 사이의 정수 쌍 - 181187 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181187?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 345.91 ms
+메모리: 87.5 MB, 시간: 23.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 03:23:12
+2024년 10월 12일 11:18:57
 
 ### 문제 설명
 
--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -2,24 +2,21 @@ class Solution {
     public long solution(int r1, int r2) {
         long r12 = (long) r1 * r1;
         long r22 = (long) r2 * r2;
-        long answer = 0;
-        
-        for (int x = 1; x < r1; x++) {
-            long x2 = (long)x*x;
-            int r1Max = (int) Math.ceil(Math.sqrt(r12 - x2));
-            int r2Max = (int) Math.sqrt(r22 - x2);
-                
-            answer += r2Max - r1Max + 1;
-
+        long answer = r2-r1+1;
+        for (long x=1; x<r1; x++) {
+            long x2 = x*x;
+            
+            long maxR1 = (long) Math.ceil(Math.sqrt(r12 - x2));
+            long maxR2 = (long) Math.floor(Math.sqrt(r22 - x2));
+            answer += maxR2 - maxR1 + 1;
         }
         
-        for (int x = r1; x < r2; x++) {
-            long x2 = (long)x*x;
-            int r2Max = (int) Math.sqrt(r22 - x2);
-            answer += r2Max;
+        for (long x=r1; x<r2; x++) {
+            long x2 = x*x;
+            long maxR2 = (long) Math.floor(Math.sqrt(r22 - x2));
+            answer += maxR2;
         }
         
-        answer += r2 - r1 + 1;
         
         return answer * 4;
     }
