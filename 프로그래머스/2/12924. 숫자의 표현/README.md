655193619570d80ea54eb0c09d1f23ebe42b47c3
박성우 <tjddn0513@gmail.com>
2024-10-12 12:20:15 +0900

    [level 2] Title: 숫자의 표현, Time: 0.11 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.46 ms
+메모리: 51.8 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 13:54:53
+2024년 10월 12일 12:20:12
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,22 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] nums = new int[n/2 + 2];
-        for (int i = 1; i < n/2 + 2; i++) {
-            nums[i] = i * (i + 1) / 2;
-        }
         int answer = 0;
-        
-        for(int right = 1; right < n/2 + 2; right++) {
-            int left = right - 1;
-            while(left > 0 && nums[right] - nums[left] < n) {
-                left--;
-            }
-            
-            if (nums[right] - nums[left] == n) {
+        for (int i =1; i<=n; i+=2) {
+            if (n%i == 0) {
                 answer++;
             }
         }
-        
-        return n > 2 ? answer + 1 : answer;
+        return answer;
     }
 }
