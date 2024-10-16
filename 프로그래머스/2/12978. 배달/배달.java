c128779cba5b96495a2599a93a291a1d54a2dda3
박성우 <tjddn0513@gmail.com>
2024-10-16 23:04:32 +0900

    [level 2] Title: 배달, Time: 4.01 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,53 +2,54 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        Queue<int[]> q = new PriorityQueue<>((x, y) -> x[1] - y[1]);
+        Map<Integer, List<int[]>> path = new HashMap<>();
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         
-        for (int[] abc : road) {
-            if (abc[2] <= K) {
-                List<int[]> route = new ArrayList<>();
-                route.add(new int[]{abc[1], abc[2]});
-                graph.merge(abc[0], route, (o, v) -> {
-                    o.addAll(route);
-                    return o;
-                });
-                
-                List<int[]> route2 = new ArrayList<>();
-                route2.add(new int[]{abc[0], abc[2]});
-                graph.merge(abc[1], route2, (o, v) -> {
-                    o.addAll(route2);
-                    return o;
-                });
-            }
-        }
-        
-        int invalidD = K + 1;
-        int[] distance = new int[N + 1];
-        Arrays.fill(distance, 2, N + 1, invalidD);
-        int answer = 1;
-        for (int[] bc : graph.get(1)) {
-            if (bc[1] <= K) {
-                q.add(bc);
-            }
-        }
-        
-        while (!q.isEmpty()) {
-            int[] bc = q.poll();
-            if (distance[bc[0]] == invalidD) {
-                distance[bc[0]] = bc[1];
-                for (int[] ac : graph.get(bc[0])) {
-                    int nextDistance = bc[1] + ac[1];
-                    if (distance[ac[0]] == invalidD && nextDistance <= K) {
-                        q.add(new int[]{ac[0], nextDistance});
-                    }
+        for (int[] r : road) {
+            if (r[2] <= K) {
+                if (!path.containsKey(r[0])) {
+                    List<int[]> route = new ArrayList<>();
+                    path.put(r[0], route);
                 }
+                path.get(r[0]).add(new int[]{r[1], r[2]});
+                
+                if (!path.containsKey(r[1])) {
+                    List<int[]> route = new ArrayList<>();
+                    path.put(r[1], route);
+                }
+                path.get(r[1]).add(new int[]{r[0], r[2]});
+            }
+        }
+        
+        int[] distance = new int[N + 1];
+        int invalidDistance = K+1;
+        Arrays.fill(distance, invalidDistance);
+    
+        pq.add(new int[]{1, 0});
+        distance[1] = 0;
+        
+        while (!pq.isEmpty()) {
+            int[] node_dist = pq.poll();
+            int node = node_dist[0];
+            int dist = node_dist[1];
+            
+            for (int[] next : path.get(node)) {
+                int next_node = next[0];
+                int next_distance = next[1] + dist;
+                
+                if (next_distance < distance[next_node]) {
+                    distance[next_node] = next_distance;
+                    pq.add(new int[]{next_node, next_distance});                 
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (distance[i] <= K) {
                 answer++;
             }
-            
         }
-
         return answer;
-        
     }
 }
