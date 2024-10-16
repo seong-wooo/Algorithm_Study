1e5302ce34b7436867382d8d3b133bf112bdfa97
박성우 <tjddn0513@gmail.com>
2024-10-16 12:22:46 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,13 +1,19 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    public int solution(int[] arr) {        
-        return Arrays.stream(arr)
-            .reduce(1, (a, b) -> a * b / gcd(a, b));
+    public int solution(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        
+        int answer = arr[0] * arr[1] / gcd(arr[0], arr[1]);
+        for (int i = 2; i < arr.length; i++) {
+            answer *= arr[i] / gcd(answer, arr[i]);
+        }
+        
+        return answer;
     }
     
     public int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        return b!=0 ? gcd(b, a % b) : a;
+        
     }
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 2] N개의 최소공배수 - 12953 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 75.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 12:14:19
+2024년 10월 16일 12:22:43
 
 ### 문제 설명
 
