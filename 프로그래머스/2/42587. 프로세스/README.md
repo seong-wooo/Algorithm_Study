6402081e795dc9537b39cdc7df0cf270610f400a
박성우 <tjddn0513@gmail.com>
2024-10-18 03:11:10 +0900

    [level 2] Title: 프로세스, Time: 1.96 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.10 ms
+메모리: 79.3 MB, 시간: 1.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 00:16:55
+2024년 10월 18일 03:11:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> pq = new PriorityQueue<>();
         Queue<int[]> q = new LinkedList<>();
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
         
         for (int i = 0; i < priorities.length; i++) {
-            pq.add(-priorities[i]);
             q.add(new int[]{priorities[i], i});
+            pq.add(priorities[i]);
         }
         
         int answer = 1;
-
         while(!q.isEmpty()) {
-            if (q.peek()[0] == -pq.peek()) {
-                int[] t = q.poll();
-                if (t[1] == location) {
+
+            if (q.peek()[0] == pq.peek()) {
+                pq.poll();
+                int[] qp = q.poll();
+                if (qp[1] == location) {
                     return answer;
                 }
-                pq.poll();
                 answer++;
-            } 
-            else {
-               q.add(q.poll()); 
+            } else {
+                q.add(q.poll());    
             }
+            
         }
-        
-        return answer;
+        return -1;
     }
 }
