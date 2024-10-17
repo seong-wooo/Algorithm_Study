6313b3a4871c45bbb05671364c324d1a71d1dac5
박성우 <tjddn0513@gmail.com>
2024-10-18 02:29:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.12 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 73.2 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 14:34:36
+2024년 10월 18일 02:29:45
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(int n, String[] words) {
-        Set<String> s = new HashSet<>();
-        s.add(words[0]);
+        Set<String> pre = new HashSet<>();
+        pre.add(words[0]);
         
         for (int i = 1; i < words.length; i++) {
-            if (words[i - 1].charAt(words[i-1].length() - 1) != words[i].charAt(0) || s.contains(words[i])) {
-                return new int[]{i % n + 1, i / n + 1};
+            if (pre.contains(words[i]) || words[i-1].charAt(words[i-1].length() - 1)  != words[i].charAt(0)) {
+                return new int[] {i % n + 1, i / n + 1};
             }
-            s.add(words[i]);
+            pre.add(words[i]);
         }
-
+        
         return new int[]{0,0};
     }
 }
