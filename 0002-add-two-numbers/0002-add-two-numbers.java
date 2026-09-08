37833550c993b7815bd143a0eff4897d02bcc269
박성우 <tjddn0513@gmail.com>
2026-09-08 14:37:45 +0900

    Time: 1 ms (100%), Space: 46.9 MB (6.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -18,15 +18,14 @@ class Solution {
 
         while (l1 != null || l2 != null || carry != 0) {
             int sum = carry;
-            if (l1 == null && l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            } else if (l1 != null && l2 == null) {
+
+            if (l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
-            } else if (l1 != null && l2 != null) {
-                sum += l1.val + l2.val;
-                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
                 l2 = l2.next;
             }
 
