23d032cbf7b2535852213f933e557d1fd83a6ab7
박성우 <tjddn0513@gmail.com>
2026-09-08 23:38:31 +0900

    Time: 22 ms (18.47%), Space: 98.3 MB (5.64%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> answer = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(n, k, new ArrayList<>());
+        return answer;
+    }
+
+    private void dfs(int n, int k, List<Integer> current) {
+        if (current.size() == k) {
+            answer.add(new ArrayList<>(current));
+            return;
+        }
+
+
+        for(int i = current.isEmpty() ? 1 : current.get(current.size() - 1) + 1; i <= n; i++) {
+            current.add(i);
+            dfs(n, k, current);
+            current.remove(current.size() - 1);
+        }
+    }
+}
