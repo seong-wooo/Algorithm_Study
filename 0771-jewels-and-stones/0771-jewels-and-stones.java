93f8bc55e719c9e8d8a1a116d431a3db2a20741e
박성우 <tjddn0513@gmail.com>
2026-09-08 19:05:56 +0900

    Time: 1 ms (58.12%), Space: 43 MB (70.24%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> s = new HashSet<>();
+
+        for (char c : jewels.toCharArray()) {
+            s.add(c);
+        }
+
+        int answer = 0;
+
+        for (char c : stones.toCharArray()) {
+            answer += s.contains(c) ?  1 : 0;    
+        }
+
+        return answer;
+    }
+}
