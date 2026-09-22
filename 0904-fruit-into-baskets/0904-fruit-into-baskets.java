d7903977422c0329a17a0286c2418c2f48d3393d
박성우 <tjddn0513@gmail.com>
2026-09-22 23:13:37 +0900

    Time: 5 ms (99.79%), Space: 72.9 MB (8.95%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,20 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         int last = -1, secondLast = -1;
-        int lastRun = 0;   // 마지막 종류가 끝에서 연속으로 나온 길이
-        int current = 0;   // 현재 유효한 윈도우 길이
+        int lastRun = 0; 
+        int current = 0;
         int answer = 0;
 
         for (int f : fruits) {
-            if (f == last || f == secondLast) {
+            if (last == f) {
                 current++;
+                lastRun++;
+            } else if (secondLast == f) {
+                current++;
+                lastRun = 1;
+                secondLast = last;
+                last = f;
             } else {
                 current = lastRun + 1;
-            }
-
-            if (f == last) {
-                lastRun++;
-            } else {
                 lastRun = 1;
                 secondLast = last;
                 last = f;
