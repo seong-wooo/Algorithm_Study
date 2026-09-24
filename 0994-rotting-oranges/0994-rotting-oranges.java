42a2b3ecd6cc567d63b32515b374f1d6fccd8907
박성우 <tjddn0513@gmail.com>
2026-09-24 13:48:40 +0900

    Time: 2 ms (87.45%), Space: 44.3 MB (24.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -5,11 +5,16 @@ class Solution {
         int[] dy = new int[]{0,0,1,-1};
         int[] dx = new int[]{1,-1,0,0};
         Queue<int[]> q = new LinkedList<>();
+        int orange = 0;
 
         for(int j = 0; j < grid.length; j++) {
             for (int i = 0; i< grid[j].length; i++) {
                 if (grid[j][i] == 2) {
                     q.add(new int[]{j, i, 0});
+                }
+
+                if (grid[j][i] == 1) {
+                    orange++;
                 }
             }
         }
@@ -28,17 +33,11 @@ class Solution {
                 if (0 <= ny && ny < grid.length && 0 <= nx && nx < grid[ny].length && grid[ny][nx] == 1) {
                     grid[ny][nx] = '2';
                     q.add(new int[]{ny,nx,time + 1});
+                    orange--;
                 }
             }
         }
 
-         for(int j = 0; j < grid.length; j++) {
-            for (int i = 0; i< grid[j].length; i++) {
-                if (grid[j][i] == 1) {
-                    return -1;
-                }
-            }
-        }
-        return answer;
+        return orange == 0 ?  answer : -1;
     }
 }
