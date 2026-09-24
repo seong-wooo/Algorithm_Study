d75c85dfbdde41850ddd1c2b0b88be5b847d1dbb
박성우 <tjddn0513@gmail.com>
2026-09-24 14:57:35 +0900

    Time: 6 ms (98.99%), Space: 49.5 MB (84.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,8 +3,11 @@ class Solution {
         Map<String, List<String>> anagram = new HashMap<>();
 
         for(String str: strs) {
-            char[] ch = str.toCharArray();
-            Arrays.sort(ch);
+            
+            char[] ch = new char[26];
+            for (char c : str.toCharArray()) {
+                ch[c - 'a']++;
+            }
             String ana = new String(ch);
 
             anagram.computeIfAbsent(ana, k -> new ArrayList<>()).add(str);
