e2fa0adc9305a10b945a9b69ce3388f331f06c9b
박성우 <tjddn0513@gmail.com>
2026-09-24 13:19:04 +0900

    Time: 7 ms (14.66%), Space: 51.1 MB (95.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,37 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
-        int[] dx = new int[]{1,-1,0,0};
-        int[] dy = new int[]{0,0,1,-1};
-
+        Queue<int[]> q = new ArrayDeque<>();
         int answer = 0;
+        int[] dy = new int[]{0,0,1,-1};
+        int[] dx = new int[]{1,-1,0,0};
+
         for (int j = 0; j < grid.length; j++) {
             for (int i = 0; i < grid[j].length; i++) {
                 if (grid[j][i] == '1') {
-                    answer++;
-                    q.offer(new int[]{j, i});
+                    q.add(new int[]{j, i});
                     grid[j][i] = '0';
+                    answer++;
 
-                    while(!q.isEmpty()) {
-                        int[] yx = q.poll();
-                        int y = yx[0];
-                        int x = yx[1];
+                    while (!q.isEmpty()) {
+                        int[] g = q.poll();
+                        int y = g[0];
+                        int x = g[1];
+
+                        grid[y][x] = '0';
 
                         for (int k = 0; k < 4; k++) {
-                            int ny = y + dy[k];
-                            int nx = x + dx[k];
+                            int ny = dy[k] + y;
+                            int nx = dx[k] + x;
 
                             if (0 <= ny && ny < grid.length && 0 <= nx && nx < grid[ny].length && grid[ny][nx] == '1') {
+                                q.add(new int[]{ny, nx});
                                 grid[ny][nx] = '0';
-                                q.offer(new int[]{ny, nx});
                             }
-                        }   
+                        }
                     }
                 }
             }
         }
-
         return answer;
     }
 }
