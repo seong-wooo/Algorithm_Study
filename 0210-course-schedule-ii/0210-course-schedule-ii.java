c4e66a1901b4bd5bf390a78d01ce637777640576
박성우 <tjddn0513@gmail.com>
2026-09-25 02:54:05 +0900

    Time: 7 ms (30.04%), Space: 47 MB (39.75%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] counter = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>(numCourses);
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            counter[p[0]]++;
+            graph.get(p[1]).add(p[0]);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (counter[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            answer.add(course);
+
+            for(int next : graph.get(course)) {
+                if (--counter[next] == 0) {
+                    q.add(next);
+                }
+            }
+        }
+
+        return answer.size() == numCourses ? answer.stream().mapToInt(Integer::intValue).toArray() : new int[] {};
+    }
+}
