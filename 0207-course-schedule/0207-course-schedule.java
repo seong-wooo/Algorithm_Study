bc49af70843b0bf7e87865b9adf9bbf073d2f78b
박성우 <tjddn0513@gmail.com>
2026-09-25 02:39:16 +0900

    Time: 9 ms (17.65%), Space: 47.4 MB (21.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,43 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> maps = new HashMap<>();
-        int[] counts = new int[numCourses];
-        Set<Integer> total = new HashSet<>();
-
-        for (int[] p : prerequisites) {
-            maps.computeIfAbsent(p[0], k -> new ArrayList<>()).add(p[1]);
-            counts[p[1]]++;
-            total.add(p[0]);
-            total.add(p[1]);
-        }
-
+        int[] counter = new int[numCourses];
+        Map<Integer, List<Integer>> courses = new HashMap<>();
         Queue<Integer> q = new LinkedList<>();
 
-        for (int i : total) {
-            if (counts[i] == 0) {
+        for (int[] p : prerequisites) {
+            counter[p[0]]++;
+            courses.computeIfAbsent(p[1], k -> new ArrayList<>()).add(p[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (counter[i] == 0) {
                 q.offer(i);
             }
         }
 
-        while(!q.isEmpty()) {
-            int c = q.poll();
-            if (!maps.containsKey(c)) {
-                continue;
-            }
-
-            List<Integer> next= maps.get(c);
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            List<Integer> next = courses.getOrDefault(course, List.of());
             for (int n : next) {
-                counts[n]--;
-                if (counts[n] == 0) {
+                if (--counter[n] == 0) {
                     q.offer(n);
                 }
             }
         }
-        for (int t : total) {
-                if (counts[t] != 0) {
-                    return false;
-                }
+
+        for (int c : counter) {
+            if (c > 0) {
+                return false;
             }
+        }
         return true;
     }
 }
