eca2192de11e40a88ea39de9ce034e8a78b7c0d8
박성우 <tjddn0513@gmail.com>
2026-09-25 16:27:47 +0900

    Time: 12 ms (84.28%), Space: 48.2 MB (12.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int left = 0;
-        char[] ch = new char[128];
+        int[] ch = new int[128];
         int answer = 0;
         for (int right = 0; right < s.length(); right++) {
             char c  = s.charAt(right);
