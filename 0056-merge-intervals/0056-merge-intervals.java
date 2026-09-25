d3333f2d97947e8e5d88c4422db36942b1fb5f5e
박성우 <tjddn0513@gmail.com>
2026-09-25 18:05:28 +0900

    Time: 8 ms (89.66%), Space: 49.3 MB (14.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
         List<int[]> answer = new ArrayList<>();
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
-
-        int[] current = intervals[0];
-
-        for (int[] node : intervals) {
-            if (current[1] >= node[0]) {
-                current[1] = (int) Math.max(current[1], node[1]);
-                continue;
+        for(int[] itv : intervals) {
+            if (answer.isEmpty()) {
+                answer.add(itv);
             } else {
-                answer.add(current);
-                current = node;
+                int[] last = answer.get(answer.size() - 1);
+                if (last[1] >= itv[0]) {
+                    last[1] = Math.max(last[1], itv[1]);
+                } else {
+                    answer.add(itv);
+                }
             }
         }
 
-        answer.add(current);
-
-        return answer.toArray(int[][]::new);
+        return answer.toArray(new int[0][]);
     }
 }
