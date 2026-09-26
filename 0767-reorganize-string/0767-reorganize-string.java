4a2eae32e2436cbf8615ffb280f92c4df0a92b20
박성우 <tjddn0513@gmail.com>
2026-09-27 01:22:48 +0900

    Time: 0 ms (100%), Space: 43.2 MB (26.27%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,52 +1,39 @@
 class Solution {
     public String reorganizeString(String s) {
+        int n = s.length();
         int[] counter = new int[26];
-        
-        char[] ch = s.toCharArray();
-        int half = (s.length() + 1) / 2;
-        
         int max = 0;
-        for (char c : ch) {
+
+        for (char c : s.toCharArray()) {
             counter[c - 'a']++;
             if (counter[c - 'a'] > counter[max]) {
                 max = c - 'a';
             }
         }
-
-        if (counter[max] > half) {
+        if (counter[max] > (n + 1) / 2) {
             return "";
         }
 
+        char[] result = new char[n];
+        int idx = 0;
 
-        char[] result = new char[s.length()];
-        char c = (char) (max + 'a');
+        idx = fill(result, idx, max, counter[max], n);   // 최빈 글자를 먼저
+        counter[max] = 0;
 
-        for (int even = 0; even < s.length(); even+=2) {
-            if (counter[c - 'a'] == 0) {
-                for (int i = 0; i < 26; i++) {
-                    if (counter[i] > 0) {
-                        c = (char) (i + 'a');
-                    }
-                }
-            }
-
-            result[even] = c;
-            counter[c - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            idx = fill(result, idx, i, counter[i], n);
         }
-
-        for (int odd = 1; odd < s.length(); odd+=2) {
-            if (counter[c - 'a'] == 0) {
-                for (int i = 0; i < 26; i++) {
-                    if (counter[i] > 0) {
-                        c = (char) (i + 'a');
-                    }
-                }
-            }
-
-            result[odd] = c;
-            counter[c - 'a']--;
-        }
-
         return new String(result);
+    }
+
+    private int fill(char[] result, int idx, int letter, int freq, int n) {
+        for (int k = 0; k < freq; k++) {
+            result[idx] = (char) ('a' + letter);
+            idx += 2;
+            if (idx >= n) {
+                idx = 1;          // 짝수 자리를 다 쓰면 홀수 자리로 넘어간다
+            }
+        }
+        return idx;
     }
 }
