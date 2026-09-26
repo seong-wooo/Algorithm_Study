cc2c357058caefe93fcf5f3809ca60de8e285087
박성우 <tjddn0513@gmail.com>
2026-09-26 23:42:07 +0900

    Time: 11 ms (88.32%), Space: 47.1 MB (73.88%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> removeIndex = new HashSet<>();
-
         Deque<Integer> stack = new ArrayDeque<>();
+        boolean[] removed = new boolean[s.length()];
 
         char[] ch = s.toCharArray();
 
@@ -13,7 +12,7 @@ class Solution {
                 stack.push(i);
             } else if (c == ')') {
                 if (stack.isEmpty()) {
-                    removeIndex.add(i);
+                    removed[i] = true;
                 } else {
                     stack.pop();
                 }
@@ -21,11 +20,11 @@ class Solution {
         }
 
         for (int index : stack) {
-            removeIndex.add(index);   
+            removed[index] = true;
         }
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < ch.length; i++) {
-            if (!removeIndex.contains(i)) {
+            if(!removed[i]) {
                 sb.append(ch[i]);
             }
         }
